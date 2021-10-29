5ee1fd1fce11af209953d4af6e70069c29cd1ded
Ankit Jain <ajain@willowinc.com>
2021-10-29 10:19:39 -0700

    Added the loader before loading todos

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -1,30 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import Todo from "./Todo";
-import { usePromiseTracker, trackPromise } from "react-promise-tracker";
+import { ClipLoader } from "react-spinners";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([]);
-  // const [filterByDateTodos, setFilterByDateTodos] = useState([]);
+  const [showLoader, setShowLoader] = useState(true);
+  let [spinnerColor, setSpinnerColor] = useState("#00001e");
 
   const URL = "https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io/get";
   const headers = {
     "X-Api-Key":
       "PMAK-5ef63db179d23c004de50751-10300736bc550d2a891dc4355aab8d7a5c",
   };
-  const { promiseInProgress } = usePromiseTracker();
-  const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
   const getTodos = async () => {
-    await sleep(2000);
     const res = await fetch(URL, { headers });
     setTodos(await res.json());
   };
 
-  function sortByIsComplete(x, y) {
+  function sortByStatus(x, y) {
     return x.isComplete === y.isComplete ? 0 : x.isComplete ? 1 : -1;
   }
-  const sortedTodos = todos.sort(sortByIsComplete);
+  const sortedTodos = todos.sort(sortByStatus);
 
   // function getInCompleteTodos() {
   //   const completedTodos = todos.filter((td) => !td.isComplete);
@@ -46,24 +44,30 @@ export default function TodoList() {
   // }
 
   useEffect(() => {
-    trackPromise(getTodos());
+    setShowLoader(true);
+    setTimeout(() => {
+      getTodos();
+      setShowLoader(false);
+    }, 1000);
   }, []);
 
   return (
     <>
-      {sortedTodos && sortedTodos.length === 0 && promiseInProgress
-        ? "loading..."
-        : sortedTodos.map((d) => {
-            return (
-              <Todo
-                key={d.id}
-                id={d.id}
-                description={d.description}
-                date={d.dueDate}
-                status={d.isComplete}
-              />
-            );
-          })}
+      {sortedTodos && sortedTodos.length === 0 && showLoader ? (
+        <ClipLoader size={100} color={spinnerColor} />
+      ) : (
+        sortedTodos.map((d) => {
+          return (
+            <Todo
+              key={d.id}
+              id={d.id}
+              description={d.description}
+              date={d.dueDate}
+              status={d.isComplete}
+            />
+          );
+        })
+      )}
     </>
   );
 }
