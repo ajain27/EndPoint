aed1c27b948d3c5c9c4bd9bb5b64c8b68b9b7560
Ankit Jain <ajain@willowinc.com>
2021-10-29 12:12:15 -0700

    isolating the colors in theme css file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import TodoList from "./components/Todo/TodoList";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
+import "./styles/theme.css";
 
 function App() {
   return (
