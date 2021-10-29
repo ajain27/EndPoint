1f4320e5874f4f139710e45bea7caa6d1d8f3db2
Ankit Jain <ajain@willowinc.com>
2021-10-29 12:20:04 -0700

    Minor change

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -9,6 +9,13 @@ export default function Todo({ id, description, date, status }) {
   const [isPending, setIsPending] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
 
+  const updateURL = `https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io/patch/${id}`;
+  const headers = {
+    "X-Api-Key":
+      "PMAK-5ef63db179d23c004de50751-10300736bc550d2a891dc4355aab8d7a5c",
+    "Content-type": "application/json",
+  };
+
   const classNames = cx(styles.todo, {
     [styles.overdue]: isOverDue === true,
     [styles.completed]: isCompleted === true,
@@ -19,12 +26,6 @@ export default function Todo({ id, description, date, status }) {
 
   function updateTodo(e) {
     setIsChecked(e.target.checked);
-    const updateURL = `https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io/patch/${id}`;
-    const headers = {
-      "X-Api-Key":
-        "PMAK-5ef63db179d23c004de50751-10300736bc550d2a891dc4355aab8d7a5c",
-      "Content-type": "application/json",
-    };
     const body = {
       isComplete: e.target.checked,
     };
